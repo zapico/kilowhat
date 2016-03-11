b327baec706522b02b8664b26712359a465ad031
Jorge Zapico <jorge@zapi.co>
2016-03-11 22:43:39 +0100

    Updated data
    Updated data and added energimyndighetens logo

--- public/data.js
+++ public/data.js
@@ -1,16 +1,16 @@
 conversions = {
 	"LED lamp" : {"id":1,"slug":"LED lamp","unit":"hours with a LED lamp on","base_value":1,"name":"LED lamp","description":"led","icon":"","category_id":2,"carbon":0.006},
-	"New Fridge" : {"id":2,"slug":"New fridge","unit":"years with a fridge on","base_value":1,"name":"New fridge","description":"led","icon":"","category_id":2,"carbon":2.42},
+	"New Fridge" : {"id":2,"slug":"New fridge","unit":"years with a fridge on","base_value":1,"name":"New fridge","description":"led","icon":"","category_id":2,"carbon":200},
 	"Bike" : {"id":3,"slug":"Bike","unit":"km driving a electric assisted bike","base_value":1,"name":"Electric bike","description":"led","icon":"/images/solarpanel.jpg","category_id":3,"carbon":0.02},
 	"car" : {"id":4,"slug":"car","unit":"km driving an electric car","base_value":1,"name":"Electric car","description":"led","icon":"","category_id":3,"carbon":4.717},
 	"kwh" : {"id":5,"slug":"kwh","unit":"kwh","base_value":1,"name":"kwh","description":"led","icon":null,"category_id":1,"carbon":1.0},
 	"solpanel" : {"id":6,"slug":"solpanel","unit":"needed square meters of solar panels in a year.","base_value":1,"name":"Solar panel","description":"led","icon":"/images/solarpanel.jpg","category_id":1,"carbon":1100.0},
 	"Old light bulb" : {"id":7,"slug":"Old light bulb","unit":"hours with a light bulb on","base_value":1,"name":"Light bulb","description":null,"icon":null,"category_id":2,"carbon":0.04},
-	"Older fridge" : {"id":8,"slug":"Older fridge","unit":"","base_value":1,"name":"Older fridge","description":null,"icon":null,"category_id":2,"carbon":1.0},
-	"tv" : {"id":9,"slug":"tv","unit":"hours watching TV","base_value":1,"name":"TV","description":null,"icon":null,"category_id":2,"carbon":1.0},
-	"wifi" : {"id":10,"slug":"wifi","unit":"days with wifi on","base_value":1,"name":"WiFi","description":"wifi","icon":null,"category_id":2,"carbon":1.0},
-	"heating" : {"id":11,"slug":"heating","unit":"","base_value":1,"name":"Heating","description":null,"icon":null,"category_id":0,"carbon":1.0},
-	"geothermal heating" : {"id":12,"slug":"geothermal heating","unit":"","base_value":1,"name":"Heating with heat pump","description":null,"icon":null,"category_id":0,"carbon":1.0},
+	"Older fridge" : {"id":8,"slug":"Older fridge","unit":"years with an older fridge on","base_value":1,"name":"Older fridge","description":null,"icon":null,"category_id":2,"carbon":500},
+	"tv" : {"id":9,"slug":"tv","unit":"hours watching TV","base_value":1,"name":"TV","description":null,"icon":null,"category_id":2,"carbon":0.067},
+	"wifi" : {"id":10,"slug":"wifi","unit":"days with wifi on","base_value":1,"name":"WiFi","description":"wifi","icon":null,"category_id":2,"carbon":0.1368},
+	"heating" : {"id":11,"slug":"heating","unit":"years heating a house with electric radiators","base_value":1,"name":"Heating","description":null,"icon":null,"category_id":0,"carbon":10600},
+	"geothermal heating" : {"id":12,"slug":"geothermal heating","unit":"years heating a house with a geothermal heatpump","base_value":1,"name":"Heating with heat pump","description":null,"icon":null,"category_id":0,"carbon":3620},
 	"coal" : {"id":13,"slug":"coal","unit":"kg of coal needed to produce that electricity","base_value":1,"name":"Coal","description":null,"icon":null,"category_id":1,"carbon":2.1198},
 	"treadmill" : {"id":14,"slug":"treadmill","unit":"hours running in a treadmill to produce that electricity","base_value":1,"name":"Treadmill","description":null,"icon":null,"category_id":1,"carbon":0.1}
 	
