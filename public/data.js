f221c60b907ac6828ac85220db79af561c4f7d41
Jorge Zapico <jorge@zapi.co>
2016-04-21 22:33:58 +0200

    Corrected some data

--- public/data.js
+++ public/data.js
@@ -4,7 +4,7 @@ conversions = {
 	"bike" : {"id":3,"slug":"bike","unit":"km driving a electric assisted bike","base_value":1,"name":"Electric bike","description":"led","icon":"/images/solarpanel.jpg","category_id":3,"carbon":0.02},
 	"car" : {"id":4,"slug":"car","unit":"km driving an electric car","base_value":1,"name":"Electric car","description":"led","icon":"","category_id":3,"carbon":4.717},
 	"kwh" : {"id":5,"slug":"kwh","unit":"kwh","base_value":1,"name":"kwh","description":"led","icon":null,"category_id":1,"carbon":1.0},
-	"solarpanel" : {"id":6,"slug":"solarpanel","unit":"needed square meters of solar panels in a year.","base_value":1,"name":"Solar panel","description":"led","icon":"/images/solarpanel.jpg","category_id":1,"carbon":1100.0},
+	"solarpanel" : {"id":6,"slug":"solarpanel","unit":"needed square meters of solar panels in a year.","base_value":1,"name":"Solar panel","description":"led","icon":"/images/solarpanel.jpg","category_id":1,"carbon":142.0},
 	"lightbulb" : {"id":7,"slug":"lightbulb","unit":"hours with a light bulb on","base_value":1,"name":"Light bulb","description":null,"icon":null,"category_id":2,"carbon":0.04},
 	"tv" : {"id":9,"slug":"tv","unit":"hours watching TV","base_value":1,"name":"TV","description":null,"icon":null,"category_id":2,"carbon":0.067},
 	"wifi" : {"id":10,"slug":"wifi","unit":"days with wifi on","base_value":1,"name":"WiFi","description":"wifi","icon":null,"category_id":2,"carbon":0.1368},
