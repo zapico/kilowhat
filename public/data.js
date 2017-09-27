caec342aace77ef410a7a6c41aedad37f40cc1be
Jorge Zapico <jorge@zapi.co>
2017-09-27 11:42:17 +0200

    data

--- public/data.js
+++ public/data.js
@@ -8,7 +8,7 @@ conversions = {
 	"lightbulb" : {"id":7,"slug":"lightbulb","unit":"hours with a light bulb on","base_value":1,"name":"Light bulb","description":null,"icon":null,"category_id":2,"carbon":0.04},
 	"tv" : {"id":9,"slug":"tv","unit":"hours watching TV","base_value":1,"name":"TV","description":null,"icon":null,"category_id":2,"carbon":0.067},
 	"wifi" : {"id":10,"slug":"wifi","unit":"days with wifi on","base_value":1,"name":"WiFi","description":"wifi","icon":null,"category_id":2,"carbon":0.1368},
-	"heating" : {"id":11,"slug":"heating","unit":"hours heating a house with electric radiators","base_value":1,"name":"Heating","description":null,"icon":null,"category_id":0,"carbon":2.4},
+	"heating" : {"id":11,"slug":"heating","unit":"hours heating a house with electric radiators","base_value":1,"name":"Heating","description":"average","icon":null,"category_id":0,"carbon":2.4},
 	"heatpump" : {"id":12,"slug":"heatpump","unit":"hours heating a house with a geothermal heatpump","base_value":1,"name":"Heating with heat pump","description":null,"icon":null,"category_id":0,"carbon":0.8},
 	"coal" : {"id":13,"slug":"coal","unit":"kg of coal needed to produce that electricity","base_value":1,"name":"Coal","description":null,"icon":null,"category_id":1,"carbon":2.1198},
 	"treadmill" : {"id":14,"slug":"treadmill","unit":"hours running in a treadmill to produce that electricity","base_value":1,"name":"Treadmill","description":null,"icon":null,"category_id":1,"carbon":0.1}
